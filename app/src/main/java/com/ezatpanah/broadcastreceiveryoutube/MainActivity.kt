4c38006feaf7063a0009b65bb765f656759c9edc
omarmasudkalam <omarmasudkalam@gmail.com>
2022-10-24 12:06:18 +0100

    Initial commit

---
+++ app/src/main/java/com/ezatpanah/broadcastreceiveryoutube/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ezatpanah.broadcastreceiveryoutube
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    lateinit var reciver : AirplaneModeChangeReceiver
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        reciver = AirplaneModeChangeReceiver()
+
+        //dynamic register
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(reciver,it)
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(reciver)
+    }
+}
---
+++ app/src/main/java/com/ezatpanah/broadcastreceiveryoutube/StaticReceiver.kt
@@ -0,0 +1,18 @@
+package com.ezatpanah.broadcastreceiveryoutube
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.widget.Toast
+
+class StaticReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show()
+        }
+        if (ConnectivityManager.CONNECTIVITY_ACTION == intent.action) {
+            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
